2eaafdd7f23a084d4a7a0f9daa156143fae3aea1
nikiforov <nikiforov@krost.ru>
2025-12-10 13:57:58 +0300

    Добавьте файлы проекта.

---
+++ CWPanelsCustomizer/Helpers/ClassTemplate.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using Autodesk.Revit.Attributes;
+//using Autodesk.Revit.DB;
+//using Autodesk.Revit.UI;
+
+//namespace CWPanelsCustomizer.Helpers
+//{
+//    [Transaction(TransactionMode.Manual)]
+//    public class ClassTemplate : IExternalCommand
+//    {
+//        public static string IS_NAME => "*Название плагина";
+//        public static string IS_DESCRIPTION => "*Что делает плагин?";
+
+
+//        public static string IS_TAB_NAME => "#BIM";
+//        public static string IS_IMAGE => "CWPanelsCustomizer.Images.a1.png";
+
+//        private SphereByPoint _sphereByPoint;
+//        private UIDocument _uidoc;
+//        private Document _doc;
+
+//        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+//        {
+//            _uidoc = commandData.Application.ActiveUIDocument;
+//            _doc = _uidoc.Document;
+//            _sphereByPoint = new SphereByPoint(_doc);
+
+//            using (TransactionGroup tg = new TransactionGroup(_doc, IS_NAME))
+//            {
+//                tg.Start();
+
+//                Method();
+
+//                tg.Assimilate();
+//            }
+
+//            return Result.Succeeded;
+//        }
+
+//        private void Method()
+//        {
+//        }
+//    }
+//}
---
+++ CWPanelsCustomizer/Helpers/SphereByPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+namespace CWPanelsCustomizer.Helpers
+{
+    internal class SphereByPoint
+    {
+        private readonly Document _doc;
+
+        public SphereByPoint(Document doc)
+        {
+            _doc = doc;
+        }
+
+        public void CreateSphereByPoint(XYZ center, string comment)
+        {
+            List<Curve> profile = new();
+            double radius = 0.2;
+            XYZ profilePlus = center + new XYZ(0, radius, 0);
+            XYZ profileMinus = center - new XYZ(0, radius, 0);
+
+            profile.Add(Line.CreateBound(profilePlus, profileMinus));
+            profile.Add(Arc.Create(profileMinus, profilePlus, center + new XYZ(radius, 0, 0)));
+
+            CurveLoop curveLoop = CurveLoop.Create(profile);
+            SolidOptions options = new SolidOptions(ElementId.InvalidElementId, ElementId.InvalidElementId);
+
+            Frame frame = new Frame(center, XYZ.BasisX, -XYZ.BasisZ, XYZ.BasisY);
+            if (Frame.CanDefineRevitGeometry(frame))
+            {
+
+                Solid sphere = GeometryCreationUtilities.CreateRevolvedGeometry(frame, new CurveLoop[] { curveLoop }, 0, 2 * Math.PI, options);
+
+                using (Transaction t = new(_doc, "create SphereByPoint"))
+                {
+                    t.Start();
+                    DirectShape ds = DirectShape.CreateElement(_doc, new ElementId(BuiltInCategory.OST_Furniture));
+                    ds.ApplicationId = "Application id";
+                    ds.ApplicationDataId = "Geometry object id";
+                    ds.SetShape(new GeometryObject[] { sphere });
+                    ds.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).Set(comment);
+                    t.Commit();
+                }
+            }
+        }
+
+        public void CreateSphereByPoint(XYZ center)
+        {
+            CreateSphereByPoint(center, string.Empty);
+        }
+    }
+}
